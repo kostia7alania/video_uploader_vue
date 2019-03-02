0fd52317d2d694c188c24e8609e33256e8afae83
kostia7alania <osetia-alania@mail.ru>
2019-03-02 18:19:17 +0300

    add global mixin(temporrary)

--- src/main.js
+++ src/main.js
@@ -14,10 +14,24 @@ import './assets/globalStyles.scss'
 import VueIziToast from 'vue-izitoast';
 Vue.use(VueIziToast, { position: "bottomRight" });
 import 'izitoast/dist/css/iziToast.min.css';
-
-
+ 
 Vue.config.productionTip = false;
 Vue.config.devtools = true;
+
+let globalMixin = {   
+  methods: {
+      getTime() {
+          let d = new Date;
+          let sec = d.getSeconds();
+          sec = sec<10?'0'+sec:sec;
+          let min = d.getMinutes();
+          min = min<10?'0'+min:min;
+          return `${d.getHours()}:${min}:${sec}`;
+      }
+  }, 
+};
+
+Vue.mixin(globalMixin);
 
 window.Vue = new Vue({
   store,
