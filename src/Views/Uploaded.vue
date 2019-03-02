862549b7f2936fa56d4556b709df86b62aee99dc
kostia7alania <osetia-alania@mail.ru>
2019-03-02 18:21:01 +0300

    work with uploadED components

--- src/Views/Uploaded.vue
+++ src/Views/Uploaded.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Already uploaded</h1>
-    <UploadedRefreshBtn @updated="shown=true"/>
+    <UploadedRefreshBtn :shown="shown" @updated="shown = true"/>
     <UploadedVideos v-if="alreadyUploaded.length" :alreadyUploaded="alreadyUploaded"/>
     <h1 v-else-if="shown">Your video-list is empty! Please, try load you video again or convert it to another format!</h1>
     <p v-else>Click the button to get the list with uploaded videos!</p>
--- src/components/uploaded/UploadedRefreshBtn.vue
+++ src/components/uploaded/UploadedRefreshBtn.vue
@@ -1,7 +1,7 @@
 <template>
-  <button class="btn btn-warning" @click="load">
-    <i :class="btn_class" class="fa" aria-hidden="true"/>
-    {{!shown?'Show':'Refresh'}} list
+  <button :class="btn_class" class="btn" @click="load">
+    <i :class="btn_icon_class" class="fa" aria-hidden="true"/>
+    {{btn_text}} 
   </button>
 </template>
 
@@ -19,12 +19,28 @@ export default {
   computed: { 
     btn_class() {
       let s = this.status;
+      if(s === 5) return 'btn-success'
+      if(s === 4) return 'btn-danger'
+      if(s === 2) return 'btn-dark loading'
+      return 'btn-warning'
+    },
+    btn_icon_class() {
+      let s = this.status;
       return  s==0?'fa-cloud-download-alt':
               s==1?'fa-sync-alt':
               s==2?'fa-splotch fa-spin':
               s==3?'fa-splotch':
               s==4?'fa-skull-crossbones':
               'fa-smile-wink'
+    },
+    btn_text() {
+      let status = this.status;
+      let shown = this.shown;
+      if(status==2) return 'Loading...';
+      if(!shown) return 'Show list';
+      if(status==4) return 'Error';
+      if(status==5) return 'Success';
+      if(shown) return 'Refresh list'
     }
   },
   methods: {
@@ -33,15 +49,21 @@ export default {
         setTimeout(() => this.status = after, 1000)
     },
     async load() {
-      if ( this.status==2 ) return;
+      let s = this.status; 
+      if ( !(s === 0 || s === 3) ) return;
       this.status = 2;
       let list = await this.$store.dispatch("getVideoList");
-      this.tmp_change_btn(list?{now:5}:{now:4}); 
-      this.$emit('updated');
+      this.tmp_change_btn( list ? {now: 5} : {now: 4} );
+      console.log('list', list)
+      if(list) this.$emit('updated'); //если ошибка, то не сообщаем, что данные получены
     }
   }
 };
 </script>
  
 <style scoped lang="scss">
+
+.loading {
+  cursor: progress;
+}
 </style>
