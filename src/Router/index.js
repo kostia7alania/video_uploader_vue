413b42fcd94c05f079ff81f2736c8d32f16c6809
kostia7alania <osetia-alania@mail.ru>
2019-03-22 16:51:09 +0300

    lazy load main page

--- src/Router/index.js
+++ src/Router/index.js
@@ -1,18 +1,28 @@
 import Vue from "vue";
 import Router from "vue-router";
 
+//import a from '@/components/uploaded/UploadedVideos'
+
 Vue.use(Router);
 
 export default new Router({
-    routes: [{
-            name: 'Home',
-            path: "/",
-            ///component: () => import("@/views/uploaded"),
-        },
-        {
-            name: "uploaded",
-            path: "/uploaded",
-            component: () => import("@/components/uploaded/UploadedVideos")
-        }
-    ]
+  routes: [
+    {
+      name: "Home",
+      path: "/",
+      component: () => import("@/Views/Upload")
+    },
+    {
+      name: "uploaded",
+      path: "/uploaded",
+      // a//,
+      component: () => import("@/Views/Uploaded")
+    }
+  ]
 });
+
+/*
+|  Building for production...
+  When setting `useBuiltIns: 'usage'`, polyfills are automatically imported when needed.
+  Please remove the `import '@babel/polyfill'` call or use `useBuiltIns: 'entry'` instead.
+\  Building for production...*/
