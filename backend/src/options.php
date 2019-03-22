3d498beb23737c21ea63c96cdddd33f0ba14c4e7
kostia7alania <osetia-alania@mail.ru>
2019-03-22 16:43:49 +0300

    implemented cors policy skipping as a separate file

---
+++ backend/src/options.php
@@ -0,0 +1,19 @@
+<?php 
+namespace Sources;
+/*if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+  if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))//may also be using PUT, PATCH, HEAD etc
+      header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
+  if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+      header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+  exit(0);
+}
+*/
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+  header('Access-Control-Allow-Origin: *');
+  header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+  header('Access-Control-Allow-Headers: token, Content-Type');
+  header('Access-Control-Max-Age: 1728000');
+  header('Content-Length: 0');
+  header('Content-Type: text/plain');
+  die();
+}
