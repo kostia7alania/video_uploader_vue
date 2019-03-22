0e4f5a8a613f0f3b4b2eef989f64cceb84fc263b
kostia7alania <osetia-alania@mail.ru>
2019-03-22 16:39:26 +0300

    add locales

--- vue.config.js
+++ vue.config.js
@@ -1,24 +1,36 @@
 module.exports = {
-    chainWebpack: config => {
-        config.module
-            .rule('vue')
-            .use('vue-loader')
-            .loader('vue-loader')
-            .tap(options => {
-                options['transformAssetUrls'] = {
-                    img: 'src',
-                    image: 'xlink:href',
-                    'b-img': 'src',
-                    'b-img-lazy': ['src', 'blank-src'],
-                    'b-card': 'img-src',
-                    'b-card-img': 'img-src',
-                    'b-carousel-slide': 'img-src',
-                    'b-embed': 'src'
-                }
+  publicPath: "./",
+  assetsDir: "./dist/",
 
-                return options
-            })
+  chainWebpack: config => {
+    config.module
+      .rule("vue")
+      .use("vue-loader")
+      .loader("vue-loader")
+      .tap(options => {
+        options["transformAssetUrls"] = {
+          img: "src",
+          image: "xlink:href",
+          "b-img": "src",
+          "b-img-lazy": ["src", "blank-src"],
+          "b-card": "img-src",
+          "b-card-img": "img-src",
+          "b-carousel-slide": "img-src",
+          "b-embed": "src"
+        };
+        return options;
+      });
+       
+  },
+
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'ru',
+      localeDir: 'locales',
+      enableInSFC: true
     }
-}
+  }
+};
 
- // https://bootstrap-vue.js.org/docs/reference/images/#vue-cli-3-support
+// https://bootstrap-vue.js.org/docs/reference/images/#vue-cli-3-support
