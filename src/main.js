78c266e6d588e54ec74e6d45f52044f813a483a6
kostia7alania <osetia-alania@mail.ru>
2019-03-29 14:39:43 +0300

    fontawesome.css--> vue components && COMMENTS with func-components && comm vue-hooks

--- src/main.js
+++ src/main.js
@@ -5,25 +5,52 @@ import store from "./Store/";
 
 import BootstrapVue from "bootstrap-vue";
 Vue.use(BootstrapVue);
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-vue/dist/bootstrap-vue.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "../node_modules/bootstrap-vue/dist/bootstrap-vue.css";
 
 import "./registerServiceWorker";
 //import "@babel/polyfill";
 import "./assets/globalStyles.scss";
-
 import router from "@/Router";
-
 import VueIziToast from "vue-izitoast";
 Vue.use(VueIziToast, { position: "bottomRight" });
-import "izitoast/dist/css/iziToast.min.css";
+import "../node_modules/izitoast/dist/css/iziToast.min.css";
+
+
+
+
+import { library, dom, config } from '@fortawesome/fontawesome-svg-core'
+import { faCoffee,faCloudDownloadAlt,faSyncAlt,faSplotch,faSkullCrossbones,faSmileWink, faSpinner,faFilm, 
+  faUpload, faAngleDoubleLeft, faTimes, 
+  faStopwatch, faBell, faCalendarAlt, faCheck,
+  faBan,faAngleRight,faAngleLeft,faTasks,faFlagCheckered,faExclamationTriangle,faExclamation,
+  faSort, faSortAlphaDown, faSortAlphaUp, faShareSquare,faTrashAlt,faStopCircle,
+  faFolderOpen,faHandPaper,faArrowUp,faArrowDown,faVideoSlash
+  }
+from '@fortawesome/free-solid-svg-icons'
+import { faYoutube, faTelegramPlane} from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+library.add( faCoffee,faCloudDownloadAlt,faSyncAlt,faSplotch,faSkullCrossbones,faSmileWink, faSpinner,faFilm, 
+  faUpload, faAngleDoubleLeft, faTimes, faYoutube, faCoffee, 
+  faStopwatch, faBell, faCalendarAlt, faTelegramPlane, faCheck,
+  faBan,faAngleRight,faAngleLeft,faTasks,faFlagCheckered,faExclamationTriangle,
+  faSort, faSortAlphaDown, faSortAlphaUp, faShareSquare,faTrashAlt,faExclamation,faStopCircle,
+  faFolderOpen,faHandPaper,faArrowUp,faArrowDown,faVideoSlash
+
+  
+    )
+
+  config.autoReplaceSvg = 'nest'//=>https://github.com/matfish2/vue-tables-2/issues/441
+dom.watch() // This will kick of the initial replacement of i to svg tags and configure a MutationObserver => https://www.npmjs.com/package/@fortawesome/vue-fontawesome
+ 
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false;
 Vue.config.devtools = true;
 
 /*
 import { withHooks, useState, useEffect } from "vue-hooks";
- 
 const Setter = withHooks( h => {
   const [sex, изменитьSEX] = useState(0);// state
   useEffect(() => { 
@@ -33,11 +60,8 @@ const Setter = withHooks( h => {
   return h("div",[h( "button",{ on: { click: () => изменитьSEX(sex + 1) },class: 'btn bg-warning'  },sex)]);
 });
 
-
-
 const Listtener = withHooks( h => {
   const width = (() => { // custom hook
-
     const [высота, изменитьВысоту ] =  useState( window.innerWidth );    //срабатывает при инициализации 
     const handleResize = () => {
       window.hand = store;
@@ -50,23 +74,40 @@ const Listtener = withHooks( h => {
       window.addEventListener("resize", handleResize);
       return () => window.removeEventListener("resize", handleResize);
     });
-
     return высота;
   })()
   return h("div", `window width is: ${width}`);
 });
 
 */
-import { hooks } from "vue-hooks";
+//import { hooks } from "vue-hooks";
 
 import i18n from "./i18n";
 import "./registerServiceWorker";
-Vue.use(hooks);
+
+//Vue.use(hooks);
 
 window.initVue = props =>
-  new Vue({
-    router,
-    store,
-    i18n,
-    render: h => h("div", [h(App, { props }) /* h(Setter), h(Listtener)*/])
+  new Vue({ router, store, i18n,
+    render(createElement) {
+      return createElement(
+          "div", 
+          { 'attrs': { class: 'render-app' }, },
+          [createElement(
+            App, 
+            { props }) 
+            /*
+            h(Setter),
+            h(Listtener)*/
+          ])
+    }
   }).$mount("#app");
+/*
+The createElement function takes three arguments of its own:
+
+1) An HTML tag name (or a component options object).
+2) A data object that corresponds to the attributes to be added to the HTML template (event listeners, class attributes, etc.).
+3) Child nodes of the parent node.
+
+-----------> https://www.fullstack.io/30-days-of-vue/day-16-render-functions-and-jsx
+*/
