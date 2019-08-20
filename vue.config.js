25421890f22eb4bfecd73d69b4777c182e504235
kostia7alania <osetia-alania@mail.ru>
2019-08-20 14:55:46 +0300

    config vue

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,31 @@
-const isDev = process.env.NODE_ENV !== "production"
+const isDev = process.env.NODE_ENV == "development"
 module.exports = {
   transpileDependencies: ['object-hash'],
   lintOnSave: isDev,
   devServer: {
-    overlay: {
-      warnings: false,
-      errors: true,
-      proxy: 'http://localhost:3000/backend/index.php' //Это скажет серверу разработки проксировать любые неизвестные запросы (запросы, которые не соответствуют статическому файлу) на адрес http://localhost:4000.
+    historyApiFallback: true, // noInfo: true,
+    overlay: true,//вывод ошибок на экран
+    open: false,
+    hot: true,
+    proxy: {
+      '/api*': {//все запросы))
+        target: 'https://apcis.tmou.org/develop/api/',//'http://localhost:3000/backend/index.php' //Это скажет серверу разработки проксировать любые неизвестные запросы (запросы, которые не соответствуют статическому файлу) на адрес http://localhost:4000.
+        secure: false,
+        changeOrigin: true,
+        onProxyRes(proxyRes, req, res) {
+          if (proxyRes.headers.location) { // Если есть заголовок со свойством location (Где храниться полный адрес запроса к локальному серверу)
+            let location = proxyRes.headers.location; // Сохраняем адрес зоголовка location из ответа в переменную location
+            console.log(`LOCATION: ${proxyRes.headers.location}`); // Выводит в консоль адрес до замены
+            location = location.replace('anyships.site', 'anyships.site:3000'); // Заменяем адрес локального сервера на адрес webpack dev server'a
+            proxyRes.headers.location = location; // Присваиваем в заголовок location подмененный адрес из переменной location
+            console.log(`REPLACED: ${proxyRes.headers.location}`); // Выводит в консоль адрес после замены
+          }
+        },
+        pathRewrite: { '^/api': '' }
+      }
     }
   },
-  publicPath: isDev ? './' : './js/video-uploader',
+  publicPath: isDev ? './' : '././js/video-manager/dist',
   assetsDir: './',   // "./", //По умолчанию: '' - Каталог (относительно outputDir) для хранения сгенерированных статических ресурсов (js, css, img, fonts).
   outputDir: 'dist',   // "dist",
   indexPath: 'index.html',  // "index.html", //умолч -'index.html'-относительно outputDir
