3ef6296d382b9824e85d420eef1698b5f1ed8f30
kostia7alania <osetia-alania@mail.ru>
2019-09-23 10:42:04 +0300

    upd

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,4 @@
 process.env.VUE_CLI_BABEL_TRANSPILE_MODULES = true;
 module.exports = {
-  presets: [
-    [
-      "@vue/app", { useBuiltIns: "entry" }
-  ]
-  ]
+  presets: [["@vue/app", { useBuiltIns: "entry" }]]
 };
