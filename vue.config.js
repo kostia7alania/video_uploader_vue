8ac064df2c05e668c8875dc602779c433cb7093c
kostia7alania <osetia-alania@mail.ru>
2019-09-25 10:42:14 +0300

    upd

--- vue.config.js
+++ vue.config.js
@@ -1,34 +1,37 @@
-const isDev = process.env.NODE_ENV == "development"
+const isDev = process.env.NODE_ENV == "development";
 module.exports = {
-  transpileDependencies: ['object-hash'],
+  transpileDependencies: ["object-hash"],
   lintOnSave: isDev,
   devServer: {
     historyApiFallback: true, // noInfo: true,
-    overlay: true,//вывод ошибок на экран
+    overlay: true, //вывод ошибок на экран
     open: false,
     hot: true,
     proxy: {
-      '/api*': {//все запросы))
-        target: 'https://apcis.tmou.org/develop/api/',//'http://localhost:3000/backend/index.php' //Это скажет серверу разработки проксировать любые неизвестные запросы (запросы, которые не соответствуют статическому файлу) на адрес http://localhost:4000.
+      "/api*": {
+        //все запросы))
+        target: "https://apcis.tmou.org/develop/api/", //'http://localhost:3000/backend/index.php' //Это скажет серверу разработки проксировать любые неизвестные запросы (запросы, которые не соответствуют статическому файлу) на адрес http://localhost:4000.
         secure: false,
         changeOrigin: true,
         onProxyRes(proxyRes, req, res) {
-          if (proxyRes.headers.location) { // Если есть заголовок со свойством location (Где храниться полный адрес запроса к локальному серверу)
+          if (proxyRes.headers.location) {
+            console.log(req, res);
+            // Если есть заголовок со свойством location (Где храниться полный адрес запроса к локальному серверу)
             let location = proxyRes.headers.location; // Сохраняем адрес зоголовка location из ответа в переменную location
             console.log(`LOCATION: ${proxyRes.headers.location}`); // Выводит в консоль адрес до замены
-            location = location.replace('anyships.site', 'anyships.site:3000'); // Заменяем адрес локального сервера на адрес webpack dev server'a
+            location = location.replace("anyships.site", "anyships.site:3000"); // Заменяем адрес локального сервера на адрес webpack dev server'a
             proxyRes.headers.location = location; // Присваиваем в заголовок location подмененный адрес из переменной location
             console.log(`REPLACED: ${proxyRes.headers.location}`); // Выводит в консоль адрес после замены
           }
         },
-        pathRewrite: { '^/api': '' }
+        pathRewrite: { "^/api": "" }
       }
     }
   },
-  publicPath: isDev ? './' : '././js/video-manager/dist',
-  assetsDir: './',   // "./", //По умолчанию: '' - Каталог (относительно outputDir) для хранения сгенерированных статических ресурсов (js, css, img, fonts).
-  outputDir: 'dist',   // "dist",
-  indexPath: 'index.html',  // "index.html", //умолч -'index.html'-относительно outputDir
+  publicPath: isDev ? "./" : "././api/video-manager/frontend/dist",
+  assetsDir: "./", // "./", //По умолчанию: '' - Каталог (относительно outputDir) для хранения сгенерированных статических ресурсов (js, css, img, fonts).
+  outputDir: "dist", // "dist",
+  indexPath: "index.html", // "index.html", //умолч -'index.html'-относительно outputDir
   filenameHashing: false,
 
   chainWebpack: config => {
@@ -60,13 +63,19 @@ module.exports = {
 
     const svgRule = config.module.rule("svg");
     svgRule.uses.clear();
-    svgRule.use("vue-svg-loader").loader("vue-svg-loader");
+    //svgRule.use('vue-svg-loader').loader('vue-svg-loader') 
+    svgRule
+      .use('babel-loader')
+      .loader('babel-loader')
+      .end()
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader')
   },
 
   pluginOptions: {
     i18n: {
-      locale: "ru",
-      fallbackLocale: "en",
+      locale: "en",
+      fallbackLocale: "ru",
       localeDir: "locales",
       enableInSFC: true
     },
@@ -75,10 +84,10 @@ module.exports = {
 
   // ...other vue-cli plugin options...
   pwa: {
-    name: 'Video uploader',
-    msTileColor: '#00FF3C',
-    appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black',
+    name: "Video uploader",
+    msTileColor: "#00FF3C",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black"
     // workboxPluginMode: 'InjectManifest'
   }
 };
